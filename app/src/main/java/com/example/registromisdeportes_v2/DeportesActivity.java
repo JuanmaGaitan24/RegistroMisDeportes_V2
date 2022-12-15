add1086719c79dc113af7c4e2f42830195038983
JuanmaGaitan24 <juanmanuelgaitan@iesflorenciopintado.es>
2022-12-15 23:12:45 +0100

    Registro mis deportes V3

--- app/src/main/java/com/example/registromisdeportes_v2/DeportesActivity.java
+++ app/src/main/java/com/example/registromisdeportes_v2/DeportesActivity.java
@@ -1,12 +1,19 @@
 package com.example.registromisdeportes_v2;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,7 +22,10 @@ public class DeportesActivity extends AppCompatActivity {
 
     public static final String NUMERO = "NUMERO";
     ListView listaDeportes;
+    Button btnAgregarDeporte;
+
     ArrayList<Deporte> deporte;
+    Deporte[] ArrayDeportes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,23 +34,72 @@ public class DeportesActivity extends AppCompatActivity {
 
         ManejadorBD manejadorBD = new ManejadorBD(this);
         listaDeportes = findViewById(R.id.ListViewDeportes);
+        btnAgregarDeporte = findViewById(R.id.buttonAgregar);
 
+        ListarDeportes(manejadorBD, listaDeportes);
+
+        btnAgregarDeporte.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+            }
+        });
+
+    }
+
+    private void ListarDeportes(ManejadorBD manejadorBD, ListView listaDeportes) {
         Cursor cursor = manejadorBD.ListarDeportes();
         deporte = new ArrayList<>();
         ArrayAdapter <String> arrayAdapter;
         List<String> list = new ArrayList<>();
+        int cont = 0;
 
         if (cursor != null && cursor.getCount() > 0){
+            ArrayDeportes = new Deporte[cursor.getCount()];
             while (cursor.moveToNext()){
                 Deporte deporte1 = new Deporte(cursor.getString(0), cursor.getString(1), cursor.getString(2));
-                String fila = deporte1.toString();
+                ArrayDeportes[cont] = deporte1;
+                cont++;
+                /*String fila = deporte1.toString();
                 list.add(fila);
-                deporte.add(deporte1);
+                deporte.add(deporte1);*/
             }
 
-            arrayAdapter = new ArrayAdapter<>(getBaseContext(), androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, list);
-            listaDeportes.setAdapter(arrayAdapter);
+            /*arrayAdapter = new ArrayAdapter<>(getBaseContext(), androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, list);
+            listaDeportes.setAdapter(arrayAdapter);*/
 
+            AdaptadorParaDeportes adaptadorParaDeportes = new AdaptadorParaDeportes(DeportesActivity.this, R.layout.lista, ArrayDeportes);
+            listaDeportes.setAdapter(adaptadorParaDeportes);
+        }
+    }
+
+    private class AdaptadorParaDeportes extends ArrayAdapter<Deporte>{
+
+        public AdaptadorParaDeportes(@NonNull Context context, int resource, @NonNull Deporte[] objects){
+            super(context, resource, objects);
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            return rellenarFila(position, convertView, parent);
+        }
+
+        private View rellenarFila(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = getLayoutInflater();
+            View mifila = inflater.inflate(R.layout.lista, parent, false);
+
+            TextView id_dep = mifila.findViewById(R.id.textViewIdDeporte);
+            TextView nom_dep = mifila.findViewById(R.id.textViewNombreDeporte);
+            TextView des_dep = mifila.findViewById(R.id.textViewDescripcionDeporte);
+
+            id_dep.setText(ArrayDeportes[position].getId());
+            nom_dep.setText(ArrayDeportes[position].getNombre());
+            des_dep.setText(ArrayDeportes[position].getDescripcion());
+
+            return mifila;
         }
     }
 
